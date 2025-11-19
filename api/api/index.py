1f830ac8542a5c2326636a9504f3e2b9328f9a1b
igortaumaturgoreal-maker <igortaumaturgoreal@gmail.com>
2025-11-19 13:32:13 -0300

    Update index.py

--- api/api/index.py
+++ api/api/index.py
@@ -1,1 +1,21 @@
+from flask import Flask, request, jsonify
+from scraper import fetch_olx
 
+app = Flask(_name_)
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({"status": "online", "message": "Car Finder API funcionando!"})
+
+@app.route("/buscar", methods=["GET"])
+def buscar():
+    termo = request.args.get("q", "")
+
+    resultados = {
+        "olx": fetch_olx(termo)
+    }
+
+    return jsonify(resultados)
+
+def handler(event, context):
+    return app(event, context)
